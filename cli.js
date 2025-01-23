7acac407014737c52dbe62cffaf67881efd31813
Scotty Jamison <theScottyJam@gmail.com>
2025-01-23 08:20:42 -0700

    Add semicolons

--- cli.js
+++ cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-import clipboardy from 'clipboardy'
-import * as codeRef from './main.js'
+import clipboardy from 'clipboardy';
+import * as codeRef from './main.js';
 
-const BRIGHT = '\x1b[1m'
-const YELLOW = '\x1b[93m'
-const RESET = '\x1b[0m'
+const BRIGHT = '\x1b[1m';
+const YELLOW = '\x1b[93m';
+const RESET = '\x1b[0m';
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 if (args.includes('-h') || args.includes('--help')) {
-  console.info('Just run this script without any arguments to generate a unique code-ref')
+  console.info('Just run this script without any arguments to generate a unique code-ref');
 } else {
-  const ref = codeRef.generate()
-  clipboardy.writeSync(ref)
-  console.info(`${BRIGHT}Your generated code-ref: ${YELLOW}${ref}${RESET}`)
-  console.info('(copied to your clipboard)')
+  const ref = codeRef.generate();
+  clipboardy.writeSync(ref);
+  console.info(`${BRIGHT}Your generated code-ref: ${YELLOW}${ref}${RESET}`);
+  console.info('(copied to your clipboard)');
 }
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const CHOICES = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  .replace(/[l1oO0]/g, '') // Some characters removed for readability
+  .replace(/[l1oO0]/g, ''); // Some characters removed for readability
 
-const arrayOfLength = length => new Array(length).fill()
+const arrayOfLength = length => new Array(length).fill();
 
 export function generate() {
-  const genChar = () => CHOICES[Math.floor(Math.random() * CHOICES.length)]
-  return '§' + arrayOfLength(5).map(genChar).join('')
+  const genChar = () => CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  return '§' + arrayOfLength(5).map(genChar).join('');
 }
